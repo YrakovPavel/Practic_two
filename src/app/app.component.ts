7f4e6f0900fe8a3a96d2b2a2f1444ffdb6259855
YrakovPavel <67166187+YrakovPavel@users.noreply.github.com>
2022-07-21 21:03:16 +0400

    Ц

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = "My-app";
+  A: number = 0;
+  B: number = 0;
+  H: number = 0;
+  answer: number = 0;
+
+  public Calculate(){
+    if (this.A < 0 || this.B < 0 || this.H < 0){
+      alert("Значения длин не могут быть меньше 0 !")
+    }
+    else{
+      this.answer = 0.5 * (this.A + this.B) * this.H;
+    }
+  }
 }
